184010ac60bb62e5bd1e19dc98aa057846fb1cbf
Bartolomeooo <bartoszkawiak@gmail.com>
2025-03-21 00:58:49 +0100

    Minor UI tweaks

--- frontend/src/components/HomePage/HomePage.tsx
+++ frontend/src/components/HomePage/HomePage.tsx
@@ -17,7 +17,7 @@ function ContextPanel() {
                 <p>Dołącz do naszej wyjątkowej inicjatywy, w której każda aukcja to szansa na wsparcie potrzebujących. Licytuj unikalne przedmioty i przeżycia, a cały dochód trafia na szczytny cel. <b>Razem możemy więcej!</b></p>
                 <p>Od 2019 roku rozświetliliśmy serca rodzin ze Szlachetnej Paczki oraz wielu potrzebujących dzieci. W zeszłym roku zebraliśmy imponujące <b>40.000 PLN</b>, co dowodzi, że razem jesteśmy w stanie zdziałać wielkie rzeczy! </p>
                 <header>Informacje ogólne</header>
-                <p>Licytacje auckji odbą się na <b>Slacku</b> <b><span style={{color: 'red'}}>#licytacje-robimy-dobro-2025</span></b> w dniach <b>21-23 listopada</b> w godzinach <b>10:00-15:00</b>. </p>
+                <p>Licytacje aukcji odbędą się na <b>Slacku</b> <b><span style={{color: 'red'}}>#licytacje-robimy-dobro-2025</span></b> w dniach <b>21-23 listopada</b> w godzinach <b>10:00-15:00</b>. </p>
                 <header>Do zobaczenia!</header>
             </div>
         </main>
