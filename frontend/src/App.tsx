2ea5d39eeb151614e5a5d36a923e804e8343d86f
MatiBukowski <mati.bukowski@icloud.com>
2025-03-21 00:07:07 +0100

    Add routing to home page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter } from 'react-router-dom';
+import {BrowserRouter } from 'react-router-dom';
 import Header from "./components/TopPanel/TopPanel";
 import Context from "./components/ContexPanel/ContextPanel";
 
--- frontend/src/components/ContexPanel/ContextPanel.tsx
+++ frontend/src/components/ContexPanel/ContextPanel.tsx
@@ -1,25 +1,14 @@
 import "./ContextPanel.css"
 import React from "react";
-import homeImage from "../../image/homeImage.svg"
+import {Route, Routes} from "react-router-dom";
+import Home from "../HomePage/HomePage";
 
 function ContextPanel() {
     return (
         <main className="ContextPanel">
-            <div className="ImageFrame">
-                <img src={homeImage} alt="HomePage Image" />
-            </div>
-            <div className="ButtonFrame">
-                <span>Sprawdź, co możesz wylicytować na aukcjach:</span>
-                <button>Zobacz aukcje</button>
-            </div>
-            <div className="TextFrame">
-                <header>Licytuj i pomagaj!</header>
-                <p>Dołącz do naszej wyjątkowej inicjatywy, w której każda aukcja to szansa na wsparcie potrzebujących. Licytuj unikalne przedmioty i przeżycia, a cały dochód trafia na szczytny cel. <b>Razem możemy więcej!</b></p>
-                <p>Od 2019 roku rozświetliliśmy serca rodzin ze Szlachetnej Paczki oraz wielu potrzebujących dzieci. W zeszłym roku zebraliśmy imponujące <b>40.000 PLN</b>, co dowodzi, że razem jesteśmy w stanie zdziałać wielkie rzeczy! </p>
-                <header>Informacje ogólne</header>
-                <p>Licytacje auckji odbą się na <b>Slacku</b> <b><span style={{color: 'red'}}>#licytacje-robimy-dobro-2025</span></b> w dniach <b>21-23 listopada</b> w godzinach <b>10:00-15:00</b>. </p>
-                <header>Do zobaczenia!</header>
-            </div>
+            <Routes>
+                <Route path="/home" element={<Home />} />
+            </Routes>
         </main>
     );
 }
