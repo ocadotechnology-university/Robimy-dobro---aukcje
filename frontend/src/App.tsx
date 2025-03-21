8318ac010415f5a76271559bc15a7eb102bca106
MatiBukowski <mati.bukowski@icloud.com>
2025-03-21 21:25:44 +0100

    Add images on sides. Fix home page resizing.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,15 +3,29 @@ import './App.css';
 import {BrowserRouter } from 'react-router-dom';
 import Header from "./components/TopPanel/TopPanel";
 import Context from "./components/ContexPanel/ContextPanel";
+import img1 from "./image/image1.jpg"
+import img2 from "./image/image2.jpg"
+import img3 from "./image/image3.jpg"
+import img4 from "./image/image4.jpg"
 
 function App() {
   return (
-    <div className="App">
-        <BrowserRouter>
-            <Header />
-            <Context />
-        </BrowserRouter>
-    </div>
+      <div className="Screen">
+          <div className="LeftPanel">
+              <img src={img4} alt="Left image" />
+          </div>
+
+          <div className="App">
+              <BrowserRouter>
+                  <Header />
+                  <Context />
+              </BrowserRouter>
+          </div>
+
+          <div className="RightPanel">
+              <img src={img3} alt="Left image" />
+          </div>
+      </div>
   );
 }
 
