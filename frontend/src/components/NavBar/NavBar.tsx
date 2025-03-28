44064e8c4f0402dc7f49fcfcae0ee8d0f4896076
Bircheses <kbrzoza03@outlook.com>
2025-03-28 12:16:14 +0100

    Add button with Material UI components.

--- frontend/src/components/NavBar/NavBar.tsx
+++ frontend/src/components/NavBar/NavBar.tsx
@@ -1,11 +1,10 @@
 import * as React from 'react';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
-import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
 import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
-import {Divider, Paper, Stack, styled} from '@mui/material';
+import {Container, Divider, Stack, Toolbar} from '@mui/material';
 import HomeIcon from "@mui/icons-material/Home";
 
 // Change to constant colors
@@ -34,73 +33,64 @@ function NavBar() {
     };
 
     return (
-        <AppBar position="static">
-            <Stack
-                flexShrink={1}
-                direction="row"
-                alignItems="center"
-                justifyContent="space-between"
-                sx={{
-                    maxHeight: "6%",
-                    minHeight: 50,
-                    backgroundColor: "white",
-                    boxShadow: "0 4px 8px rgba(214, 214, 200, 0.5)",
-                    position: "relative",
-                    zIndex: 10,
-                    px: 2
-                }}
-            >
-                {/*<Stack flexShrink={1} direction="row" alignItems="center" justifyContent="flex-start" spacing={0.5} sx={{ cursor: "pointer" }}>
-                    <HomeIcon sx={{color: "rgba(236, 142, 6, 1)"}} fontSize="large"/>
+        <AppBar position="static" sx={{backgroundColor: "white"}}>
+            <Container maxWidth="lg">
+                <Toolbar disableGutters>
+                    {/*Logo*/}
+                    <Box flexGrow={0}>
+                        <Typography fontSize="15px" color="black" fontWeight="bold"
+                                    fontFamily="Inter">
+                            <Stack sx={{cursor: "pointer"}} direction="row" spacing={1} alignItems="center">
+                                <HomeIcon fontSize="large" sx={{color: buttonColor}}/>
+                                <Stack direction="column">
+                                    <Box>Robimy</Box>
+                                    <Box>Dobro</Box>
+                                </Stack>
+                                <Divider sx={{borderColor: buttonColor}} orientation="vertical" variant="middle"
+                                         flexItem/>
+                                <Box>2025</Box>
+                            </Stack>
+                        </Typography>
+                    </Box>
 
-                    <Typography>
-                        <Stack Array>
+                    <Box flexGrow={1}></Box>
 
-                        </Stack>
-                    </Typography>
+                    {/*Przycisk*/}
+                    <Box flexGrow={0}>
+                        <Button variant="contained"
+                            sx={{
+                                borderRadius: "45px",
+                                backgroundColor: buttonColor,
+                                fontWeight: 600,
+                                textTransform: "none"
+                            }}
+                        >
+                            Dodaj aukcję
+                        </Button>
+                    </Box>
 
-                    <Stack spacing={-0.7}>
-                        <Typography fontSize="14px" color="black" fontWeight={750}>Robimy</Typography>
-                        <Typography fontSize="14px" color="black" fontWeight={750} mt={0.5}>Dobro</Typography>
-                    </Stack>
-                    <Typography fontSize="25px" color="rgba(236, 142, 6, 1.0)">|</Typography>
-                    <Typography fontSize="14px" color="black" fontWeight={750}>2025</Typography>
-                </Stack>*/}
 
+                    {/*<Box mr={2}>
+                        <Button
+                            variant="contained"
+                            sx={{
+                                borderRadius: "45px",
+                                backgroundColor: "rgba(236, 142, 6, 1)",
+                                fontWeight: 600,
+                                textTransform: "none",
+                                boxShadow: "0 4px 8px rgba(0, 0, 0, 0.3)",
+                                '&:hover': {backgroundColor: "rgba(236, 142, 6, 1)"}
+                            }}
+                        >
+                            Dodaj aukcję
+                        </Button>
+                    </Box>
 
-                {/*Logo*/}
-                <Typography sx={{ cursor: "pointer" }} fontSize="15px" color="black" fontWeight="bold" fontFamily="Inter">
-                    <Stack direction="row" spacing={1} alignItems="center" >
-                        <HomeIcon fontSize="large" sx={{color:buttonColor}} />
-                        <Stack direction="column">
-                            <Box>Robimy</Box>
-                            <Box>Dobro</Box>
-                        </Stack>
-                        <Divider sx={{borderColor:buttonColor}} orientation="vertical" variant="middle" flexItem/>
-                        <Box>2025</Box>
-                    </Stack>
-                </Typography>
-
-                <Box mr={2}>
-                    <Button
-                        variant="contained"
-                        sx={{
-                            borderRadius: "45px",
-                            backgroundColor: "rgba(236, 142, 6, 1)",
-                            fontWeight: 600,
-                            textTransform: "none",
-                            boxShadow: "0 4px 8px rgba(0, 0, 0, 0.3)",
-                            '&:hover': {backgroundColor: "rgba(236, 142, 6, 1)"}
-                        }}
-                    >
-                        Dodaj aukcję
-                    </Button>
-                </Box>
-
-                <Box>
-                    <Avatar/>
-                </Box>
-            </Stack>
+                    <Box>
+                        <Avatar/>
+                    </Box>*/}
+                </Toolbar>
+            </Container>
         </AppBar>
     );
 }
