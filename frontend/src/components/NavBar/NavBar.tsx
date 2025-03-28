b1a6e9e76598966d7d71d3614a04e7d0f88e2d03
Bircheses <kbrzoza03@outlook.com>
2025-03-28 13:20:04 +0100

    Add automatic resizing when the window is too small

--- frontend/src/components/NavBar/NavBar.tsx
+++ frontend/src/components/NavBar/NavBar.tsx
@@ -4,45 +4,30 @@ import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
-import {Container, Divider, Stack, Toolbar} from '@mui/material';
+import {Container, Divider, IconButton, Stack, Toolbar} from '@mui/material';
 import HomeIcon from "@mui/icons-material/Home";
+import AddOutlinedIcon from '@mui/icons-material/AddOutlined';
 
-// Change to constant colors
+// Change to theme colors
 const buttonColor = "rgba(236, 142, 6, 1.0)";
 
-const pages = ['Products', 'Pricing', 'Blog'];
-const settings = ['Moje Aukcje', 'Wyloguj'];
-
 function NavBar() {
-    const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(null);
-    const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(null);
-
-    const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
-        setAnchorElNav(event.currentTarget);
-    };
-    const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
-        setAnchorElUser(event.currentTarget);
-    };
-
-    const handleCloseNavMenu = () => {
-        setAnchorElNav(null);
-    };
-
-    const handleCloseUserMenu = () => {
-        setAnchorElUser(null);
-    };
-
     return (
         <AppBar position="static" sx={{backgroundColor: "white"}}>
-            <Container maxWidth="lg">
+            <Container maxWidth="xl">
                 <Toolbar disableGutters>
-                    <Stack flexGrow={1} direction="row" spacing={2}>
+
+                    {/*Middle Sizing*/}
+                    <Stack flexGrow={1} direction="row" spacing={2} sx={{display: {xs:"none", sm:"flex", md:"flex"}}}>
                         {/*Logo*/}
                         <Box flexGrow={0}>
                             <Typography fontSize="15px" color="black" fontWeight="bold"
                                         fontFamily="Inter">
                                 <Stack sx={{cursor: "pointer"}} direction="row" spacing={1} alignItems="center">
-                                    <HomeIcon fontSize="large" sx={{color: buttonColor}}/>
+                                    <HomeIcon sx={{
+                                        width: 36.5,
+                                        height: 36.5,
+                                        color: buttonColor}}/>
                                     <Stack direction="column">
                                         <Box>Robimy</Box>
                                         <Box>Dobro</Box>
@@ -56,8 +41,8 @@ function NavBar() {
 
                         <Box flexGrow={1}></Box>
 
-                        {/*Przycisk*/}
-                        <Box>
+                        {/*Button*/}
+                        <Box sx={{display:'flex', justifyContent: 'center', alignItems: 'center'}}>
                             <Button variant="contained"
                                     sx={{
                                         borderRadius: "45px",
@@ -70,9 +55,41 @@ function NavBar() {
                             </Button>
                         </Box>
 
-                        {/*Konto*/}
-                        <Box>
-                            <Avatar sx={{width: 37, height: 37}}></Avatar>
+                        {/*Account*/}
+                        <Box sx={{display:'flex', justifyContent: 'center', alignItems: 'center'}}>
+                            <Avatar sx={{width: 36.5, height: 36.5}}></Avatar>
+                        </Box>
+                    </Stack>
+
+                    {/*Small Sizing*/}
+                    <Stack flexGrow={1} direction="row" spacing={2} sx={{display: {xs:"flex", sm:"none", md:"none"}}}>
+                        {/*Logo*/}
+                        <Box flexGrow={0} sx={{display:'flex', justifyContent: 'center', alignItems: 'center'}}>
+                                <HomeIcon sx={{width: 36.5, height: 36.5, color: buttonColor, cursor: "pointer"}}/>
+                        </Box>
+
+                        <Box flexGrow={1}></Box>
+
+                        {/*Button*/}
+                        <Box sx={{display:'flex', justifyContent: 'center', alignItems: 'center'}}>
+                            <IconButton
+                                    sx={{
+                                        width: 36.5,
+                                        height: 36.5,
+                                        borderRadius: "45px",
+                                        backgroundColor: buttonColor,
+                                        fontWeight: 600,
+                                        textTransform: "none",
+                                        padding: "none"
+                                    }}
+                            >
+                                <AddOutlinedIcon fontSize="medium" sx={{color:"white"}}/>
+                            </IconButton>
+                        </Box>
+
+                        {/*Account*/}
+                        <Box sx={{display:'flex', justifyContent: 'center', alignItems: 'center'}}>
+                            <Avatar sx={{width: 36.5, height: 36.5}}></Avatar>
                         </Box>
                     </Stack>
                 </Toolbar>
