b9b61afe8c86a23250beb21203e90e0e4829fb77
MatiBukowski <mati.bukowski@icloud.com>
2025-04-01 01:21:02 +0200

    Nearly complete

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,39 +1,57 @@
-import React from 'react';
-import './App.css';
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import React, {useEffect, useState} from 'react';
+import {BrowserRouter, Routes, Route, Navigate} from 'react-router-dom';
 import Header from "./components/NavBar/NavBar";
-import img1 from "./image/image3.jpg"
-import img2 from "./image/image4.jpg"
+import img1 from "./image/image3.jpg";
+import img2 from "./image/image4.jpg";
 import Auth from "./components/GoogleLogin/Auth";
 import Home from "./components/HomePage/HomePage";
 import AddAuction from "./components/AddPage/AddPage";
-
-
+import {Box, Container, Stack} from "@mui/material";
 
 function App() {
-  return (
-      <div className="Screen">
-          <div className="LeftPanel">
-              <img src={img2} alt="Left image" />
-          </div>
+    const [windowWidth, setWindowWidth] = useState(window.innerWidth);
 
-          <div className="App">
-              <BrowserRouter>
-                  <Header />
-                  <Routes>
-                      <Route path="/" element={<Navigate to="/auth" />} />
-                      <Route path="/auth" element={<Auth />} />
-                      <Route path="/home" element={<Home />} />
-                      <Route path="/add" element={<AddAuction />} />
-                  </Routes>
-              </BrowserRouter>
-          </div>
+    useEffect(() => {
+        const handleResize = () => {
+            setWindowWidth(window.innerWidth);
+        };
 
-          <div className="RightPanel">
-              <img src={img1} alt="Left image" />
-          </div>
-      </div>
-  );
+        window.addEventListener("resize", handleResize);
+        handleResize();
+
+        return () => window.removeEventListener("resize", handleResize);
+    }, []);
+
+    return (
+        <Container disableGutters sx={{minWidth:'100%', height:'100%'}}>
+            <Stack direction="row" justifyContent="center" sx={{ height: "100%" }}>
+
+                {windowWidth > 1500 && (
+                    <Box height="100%">
+                        <img src={img2} alt="Left image" style={{height: "100%", objectFit: 'cover'}} />
+                    </Box>
+                )}
+
+                <Box height="100%" sx={{minWidth: windowWidth > 1500 ? '1500px' : '100%'}}>
+                    <BrowserRouter >
+                        <Header/>
+                        <Routes>
+                            <Route path="/" element={<Navigate to="/auth"/>}/>
+                            <Route path="/auth" element={<Auth/>}/>
+                            <Route path="/home" element={<Home/>}/>
+                            <Route path="/add" element={<AddAuction/>}/>
+                        </Routes>
+                    </BrowserRouter>
+                </Box>
+
+                {windowWidth > 1500 && (
+                    <Box height="100%">
+                        <img src={img1} alt="Right image" style={{height: "100%", objectFit: 'cover'}}/>
+                    </Box>
+                )}
+            </Stack>
+        </Container>
+    );
 }
 
 export default App;
