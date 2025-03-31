5cfdad48b6757e63d8fb1696a7d6877e031e1b33
Bircheses <kbrzoza03@outlook.com>
2025-03-31 14:11:23 +0200

    Change to react function components.

--- frontend/src/components/NavBar/NavBar.tsx
+++ frontend/src/components/NavBar/NavBar.tsx
@@ -1,75 +1,56 @@
 import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Typography from '@mui/material/Typography';
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import {Container, Divider, IconButton, Stack, Toolbar, useMediaQuery, useTheme} from '@mui/material';
+import {AppBar, Avatar, Box, Button, Container, Divider, IconButton, Stack, Toolbar, Typography, useMediaQuery, useTheme} from "@mui/material";
 import HomeIcon from "@mui/icons-material/Home";
 import AddOutlinedIcon from '@mui/icons-material/AddOutlined';
 import {CenteredBoxStyle, ButtonStyle, ButtonColor, AvatarStyle, IconButtonStyle, HomeIconStyle} from "./NavBar.styles";
 
-function Account() {
-    return (
-        <Box sx={CenteredBoxStyle}>
-            <Avatar sx={AvatarStyle}></Avatar>
-        </Box>
-    )
-}
+const Account = () => (
+    <Box sx={CenteredBoxStyle}>
+        <Avatar sx={AvatarStyle} />
+    </Box>
+);
 
-function ButtonSmall() {
-    return (
-        <Box sx={CenteredBoxStyle}>
-            <IconButton sx={IconButtonStyle}>
-                <AddOutlinedIcon fontSize="medium" sx={{color: "white"}}/>
-            </IconButton>
-        </Box>
-    )
-}
+const ButtonSmall = () => (
+    <Box sx={CenteredBoxStyle}>
+        <IconButton sx={IconButtonStyle}>
+            <AddOutlinedIcon fontSize="medium" sx={{ color: "white" }} />
+        </IconButton>
+    </Box>
+);
 
-function LogoSmall() {
-    return (
-        <Box flexGrow={0} sx={CenteredBoxStyle}>
-            <HomeIcon sx={HomeIconStyle}/>
-        </Box>
-    )
-}
+const LogoSmall = () => (
+    <Box flexGrow={0} sx={CenteredBoxStyle}>
+        <HomeIcon sx={HomeIconStyle} />
+    </Box>
+);
 
-function ButtonMedium() {
-    return (
-        <Box sx={CenteredBoxStyle}>
-            <Button variant="contained" sx={ButtonStyle}>
-                Dodaj aukcję
-            </Button>
-        </Box>
-    )
-}
+const ButtonMedium = () => (
+    <Box sx={CenteredBoxStyle}>
+        <Button variant="contained" sx={ButtonStyle}>
+            Dodaj aukcję
+        </Button>
+    </Box>
+);
 
-function LogoMedium() {
-    return (
-        <Box flexGrow={0}>
-            <Typography fontSize="15px" color="black" fontWeight="bold" fontFamily="Inter">
-                <Stack sx={{cursor: "pointer"}} direction="row" spacing={1} alignItems="center">
-                    <HomeIcon sx={HomeIconStyle}/>
-                    <Stack direction="column">
-                        <Box>Robimy</Box>
-                        <Box>Dobro</Box>
-                    </Stack>
-                    <Divider sx={{borderColor: ButtonColor}} orientation="vertical" variant="middle" flexItem/>
-                    <Box>2025</Box>
+const LogoMedium = () => (
+    <Box flexGrow={0}>
+        <Typography fontSize="15px" color="black" fontWeight="bold" fontFamily="Inter">
+            <Stack sx={{ cursor: "pointer" }} direction="row" spacing={1} alignItems="center">
+                <HomeIcon sx={HomeIconStyle} />
+                <Stack direction="column">
+                    <Box>Robimy</Box>
+                    <Box>Dobro</Box>
                 </Stack>
-            </Typography>
-        </Box>
-    )
-}
+                <Divider sx={{ borderColor: ButtonColor }} orientation="vertical" variant="middle" flexItem />
+                <Box>2025</Box>
+            </Stack>
+        </Typography>
+    </Box>
+);
 
-function PlaceHolder() {
-    return (
-        <Box flexGrow={1}></Box>
-    )
-}
+const PlaceHolder = () => <Box flexGrow={1} />;
 
-function NavBar() {
+const NavBar = () => {
     const theme = useTheme();
     const isSmallScreen = useMediaQuery(theme.breakpoints.down("sm"));
     const isMediumScreen = useMediaQuery(theme.breakpoints.down("md"));
@@ -100,6 +81,6 @@ function NavBar() {
             </Container>
         </AppBar>
     );
-}
+};
 
 export default NavBar;
