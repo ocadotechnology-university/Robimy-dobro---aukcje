11036e208cd922afc86ca578be0e457a4ba4d2ea
MatiBukowski <mati.bukowski@icloud.com>
2025-04-02 22:30:12 +0200

    Divide App.tsx into separate component functions.

--- frontend/src/main/App.tsx
+++ frontend/src/main/App.tsx
@@ -7,6 +7,46 @@ import Auth from "../components/GoogleLogin/Auth";
 import Home from "../components/HomePage/HomePage";
 import AddAuction from "../components/AddPage/AddPage";
 import {Box, Container, Stack, useMediaQuery, useTheme} from "@mui/material";
+import Typography from "@mui/material/Typography";
+import {HomeHeadersStyle, HomeTextStyle} from "../components/HomePage/HomePage.styles";
+
+const LeftPanel = ({isLarge}: { isLarge: boolean }) => {
+    if (!isLarge) return null;
+
+    return (
+        <Box height="100%">
+            <img src={img2} alt="Left image"
+                 style={{height: '100%', objectFit: 'cover', display: 'block', overflow: "hidden"}}/>
+        </Box>
+    );
+}
+
+const MiddlePanel =({isLarge}: { isLarge: boolean }) => {
+    return(
+        <Box height="100%" sx={{minWidth: isLarge ? '1200px' : '100%'}}>
+            <BrowserRouter>
+                <Header/>
+                <Routes>
+                    <Route path="/" element={<Navigate to="/auth"/>}/>
+                    <Route path="/auth" element={<Auth/>}/>
+                    <Route path="/home" element={<Home/>}/>
+                    <Route path="/add" element={<AddAuction/>}/>
+                </Routes>
+            </BrowserRouter>
+        </Box>
+    );
+}
+
+const RightPanel = ({isLarge}: { isLarge: boolean }) => {
+    if (!isLarge) return null;
+
+    return (
+        <Box height="100%">
+            <img src={img1} alt="Left image"
+                 style={{height: '100%', objectFit: 'cover', display: 'block', overflow: "hidden"}}/>
+        </Box>
+    );
+}
 
 function App() {
     const [windowWidth, setWindowWidth] = useState(window.innerWidth);
@@ -25,32 +65,11 @@ function App() {
     }, []);
 
     return (
-        <Container disableGutters sx={{minWidth:'100%', height:'100%'}}>
-            <Stack direction="row" justifyContent="center" sx={{ height: "100%" }}>
-
-                {isLarge && (
-                    <Box height="100%">
-                        <img src={img2} alt="Left image" style={{height: "100%", objectFit: 'cover', display: 'block', overflow: "hidden"}}/>
-                    </Box>
-                )}
-
-                <Box height="100%" sx={{minWidth: isLarge ? '1200px' : '100%' }}>
-                    <BrowserRouter >
-                        <Header/>
-                        <Routes>
-                            <Route path="/" element={<Navigate to="/auth"/>}/>
-                            <Route path="/auth" element={<Auth/>}/>
-                            <Route path="/home" element={<Home/>}/>
-                            <Route path="/add" element={<AddAuction/>}/>
-                        </Routes>
-                    </BrowserRouter>
-                </Box>
-
-                {isLarge && (
-                    <Box height="100%" >
-                        <img src={img1} alt="Right image" style={{height: "100%", objectFit: 'cover', display: 'block', overflow: "hidden"}}/>
-                    </Box>
-                )}
+        <Container disableGutters sx={{minWidth: '100%', height: '100%'}}>
+            <Stack direction="row" justifyContent="center" sx={{height: "100%"}}>
+                <LeftPanel isLarge={isLarge}/>
+                <MiddlePanel isLarge={isLarge}/>
+                <RightPanel isLarge={isLarge}/>
             </Stack>
         </Container>
     );
