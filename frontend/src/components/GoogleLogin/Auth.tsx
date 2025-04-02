eb28e15a4baa7bbfd70227133f8039a29f0d9c04
Bircheses <kbrzoza03@outlook.com>
2025-04-02 23:22:55 +0200

    Add custom login button

--- frontend/src/components/GoogleLogin/Auth.tsx
+++ frontend/src/components/GoogleLogin/Auth.tsx
@@ -1,10 +1,28 @@
 import {GoogleLogin} from "@react-oauth/google";
+import {useGoogleLogin} from "@react-oauth/google";
+import Container from "@mui/material/Container";
+import Button from "@mui/material/Button";
+import GoogleIcon from '@mui/icons-material/Google';
 
 export default function Auth() {
+    const login = useGoogleLogin({
+        onSuccess: (credentialResponse) => console.log(credentialResponse)
+    })
+
     return (
-        <GoogleLogin
-            onSuccess={(credentialResponse) => console.log(credentialResponse)}
-            onError={() => console.log("Login failed")}
-        />
-    )
+        <Container sx={{height: '100%', width: '100%', display: "flex", alignItems: "center", justifyContent: "center"}}>
+                <Button startIcon={<GoogleIcon/>} variant="outlined" onClick={() => login()}
+                        sx={{
+                            color: "black",
+                            borderColor: "Grey",
+                            background:"white"}}
+                >
+                    Continue with google
+                </Button>
+                {/*<GoogleLogin
+                    onSuccess={(credentialResponse) => console.log(credentialResponse)}
+                    onError={() => console.log("Login failed")}
+                />*/}
+        </Container>
+)
 }
