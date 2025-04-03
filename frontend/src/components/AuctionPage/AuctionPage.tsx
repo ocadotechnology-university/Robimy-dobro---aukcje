f4b26f5b50d9273518a9819f63b47574cf0c7f42
MatiBukowski <mati.bukowski@icloud.com>
2025-04-03 10:07:45 +0200

    Add AuctionPage layout with separate Filters and AuctionsList functions.

---
+++ frontend/src/components/AuctionPage/AuctionPage.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import CssBaseline from '@mui/material/CssBaseline';
+import Container from '@mui/material/Container';
+import Stack from '@mui/material/Stack';
+import Box from '@mui/material/Box';
+import Filters from './Filters';
+import AuctionsList from "./AuctionsList";
+
+const AuctionPage = () => {
+    return (
+        <React.Fragment>
+            <CssBaseline />
+            <Container maxWidth="md" sx={{ backgroundColor:'white' }}>
+                <Stack direction="row" justifyContent="space-between">
+                    <Filters />
+                    <AuctionsList />
+                </Stack>
+
+            </Container>
+        </React.Fragment>
+    );
+}
+
+export default AuctionPage;
---
+++ frontend/src/components/AuctionPage/Filters.tsx
@@ -0,0 +1,11 @@
+import Stack from '@mui/material/Stack';
+
+const Filters = () => {
+    return (
+        <Stack sx={{backgroundColor:'red'}}>
+
+        </Stack>
+    );
+}
+
+export default Filters;
