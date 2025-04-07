e2960baf02266b7766deda80eb92d40d740eef2f
Bircheses <kbrzoza03@outlook.com>
2025-04-07 12:26:40 +0200

    Add axios to post google access token to backend endpoint.

--- frontend/src/components/GoogleLogin/Auth.tsx
+++ frontend/src/components/GoogleLogin/Auth.tsx
@@ -2,6 +2,7 @@ import {GoogleLogin} from "@react-oauth/google";
 import {useNavigate} from "react-router-dom";
 import React from "react";
 import {Stack, Container, Typography} from "@mui/material";
+import axios from "axios";
 
 export default function Auth() {
     const navigate = useNavigate();
@@ -13,9 +14,27 @@ export default function Auth() {
                         Musisz się zalogować, przy pomocy mail'a służbowego aby kontynuować:
                     </Typography>
                     <GoogleLogin
-                        onSuccess={(credentialResponse) =>{
-                            console.log(credentialResponse);
-                            navigate("/home");
+                        onSuccess={async (credentialResponse) =>{
+
+                            //Temporary setup to see if it works
+                           /* console.log(credentialResponse);
+                            navigate("/home");*/
+
+                            //This will send the google accessToken to the backend, comment the try catch and uncomment above to see it working
+                            try {
+                                const res = await axios.post(
+                                    "api/auth/google",
+                                    { token: credentialResponse.credential },
+                                    { withCredentials: true }
+                                );
+
+                                //Sending the access token to useAuth hook here with res.data.accessToken
+
+                                console.log(credentialResponse);
+                                navigate("/home");
+                            } catch (error) {
+                                console.log("There was a problem with getting an access token");
+                            }
                         }}
                         onError={() => console.log("Login failed")}
                     />
