c37deb790232d2b7203b7e12f33d206e4881faba
MatiBukowski <mati.bukowski@icloud.com>
2025-04-10 21:40:40 +0200

    Add custom components.

--- frontend/src/components/NavBar/NavBar.styles.tsx
+++ frontend/src/components/NavBar/NavBar.styles.tsx
@@ -1,24 +1,26 @@
+import Box from "@mui/material/Box";
+import { styled } from "@mui/material";
+import IconButton, { IconButtonProps } from '@mui/material/IconButton';
+import { LinkProps } from 'react-router-dom';
+import HomeIcon from "@mui/icons-material/Home";
+
 export const ButtonColor = "rgba(236, 142, 6, 1.0)";
+type IconButtonLinkProps = IconButtonProps & LinkProps;
 
-export const AvatarStyle = {
-    width: 36.5,
-    height: 36.5
-}
-
-export const CenteredBoxStyle = {
+export const CenteredBox = styled(Box)({
     display: 'flex',
     justifyContent: 'center',
-    alignItems: 'center'
-}
+    alignItems: 'center',
+});
 
-export const HomeIconStyle = {
+export const HomeIconComponent = styled(HomeIcon)({
     width: 36.5,
     height: 36.5,
     color: ButtonColor,
     cursor: "pointer"
-}
+});
 
-export const IconButtonStyle = {
+export const IconButtonComponent = styled(IconButton)<IconButtonLinkProps>({
     width: 36.5,
     height: 36.5,
     borderRadius: "45px",
@@ -26,6 +28,11 @@ export const IconButtonStyle = {
     fontWeight: 600,
     textTransform: "none",
     padding: "none"
+});
+
+export const AvatarStyle = {
+    width: 36.5,
+    height: 36.5
 }
 
 export const ButtonStyle = {
