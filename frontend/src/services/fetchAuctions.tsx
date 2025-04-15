6ff3883044814d65c9284d9fce1b3653f7751130
Bartolomeooo <bartoszkawiak@gmail.com>
2025-04-15 20:50:41 +0200

    Create auction fetching service

---
+++ frontend/src/services/fetchAuctions.tsx
@@ -0,0 +1,16 @@
+import axios from "axios";
+import { Auction } from "../components/AuctionPage/Auction";
+
+export interface AuctionFilters {
+    statuses?: string[];
+    myAuctions?: boolean;
+    followed?: boolean;
+    dates?: string[];
+}
+
+export const fetchAuctions = async (filters: AuctionFilters): Promise<Auction[]> => {
+    const response = await axios.get<Auction[]>("/api/auction", {
+        params: filters,
+    });
+    return response.data;
+};
