34c06aeabcf94887e8455ccd1630d640a7e189a3
MatiBukowski <mati.bukowski@icloud.com>
2025-04-24 22:15:04 +0200

    Add image upload from our files and basic cropper usage.

--- frontend/src/components/common/ImageUploadBox.tsx
+++ frontend/src/components/common/ImageUploadBox.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import Box from '@mui/material/Box';
 import UploadIcon from '@mui/icons-material/Upload';
+import { useState, useCallback } from 'react'
+import Cropper from 'react-easy-crop'
 
 const boxStyle = {
     alignSelf: 'center',
@@ -19,10 +21,58 @@ const boxStyle = {
     },
 };
 
-const ImageUploadBox = () => (
-    <Box component="label" sx={boxStyle}>
-        <UploadIcon fontSize="large" sx={{ color: '#666' }} />
-    </Box>
-);
+const ImageUploadBox = () => {
+    const [imageSrc, setImageSrc] = useState<string | null>(null);
+    const [crop, setCrop] = useState({x: 0, y: 0})
+    const [zoom, setZoom] = useState(1)
 
+    const onCropComplete = (croppedArea: any, croppedAreaPixels: any) => {
+        console.log(croppedArea, croppedAreaPixels)
+    }
+
+    const onFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const file = e.target.files?.[0];
+        const allowedTypes = ['image/jpeg', 'image/png', 'image/jpg'];
+
+        if (file && !allowedTypes.includes(file.type)) {
+            alert("Niepoprawne rozszerzenie wybranego pliku");
+            e.target.value = '';
+            return;
+        }
+
+        if (file) {
+            const url = URL.createObjectURL(file);
+            setImageSrc(url);
+        }
+    };
+
+    return (
+        <Box sx={{ display: 'flex', flexDirection: 'column', gap: 2 }}>
+            <Box component="label" sx={boxStyle}>
+                <input
+                    accept="image/*"
+                    style={{ display: 'none' }}
+                    id="input-image-button"
+                    type="file"
+                    onChange={onFileChange}
+                />
+                <UploadIcon fontSize="large" sx={{ color: '#666' }} />
+            </Box>
+
+            {imageSrc && (
+                <Box sx={{ width: '100%', height: 300, position: 'relative' }}>
+                    <Cropper
+                        image={imageSrc}
+                        crop={crop}
+                        zoom={zoom}
+                        aspect={4 / 3}
+                        onCropChange={setCrop}
+                        onCropComplete={onCropComplete}
+                        onZoomChange={setZoom}
+                    />
+                </Box>
+            )}
+        </Box>
+    );
+};
 export default ImageUploadBox;
