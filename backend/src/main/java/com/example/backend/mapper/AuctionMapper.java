da683a805582de89ceb0af140d5d7d7c791cd1cd
Bartolomeooo <bartoszkawiak@gmail.com>
2025-04-26 00:20:22 +0200

    Handle null values when mapping Auction to AuctionGetDto

--- backend/src/main/java/com/example/backend/mapper/AuctionMapper.java
+++ backend/src/main/java/com/example/backend/mapper/AuctionMapper.java
@@ -17,13 +17,13 @@ public class AuctionMapper {
 
         auctionGetDto.setTitle(auction.getTitle());
         auctionGetDto.setCity(auction.getCity());
-        auctionGetDto.setDate(auction.getAuctionDate().toString());
+        auctionGetDto.setDate(auction.getAuctionDate() == null ? null : auction.getAuctionDate().toString());
         auctionGetDto.setDescription(auction.getDescription());
         auctionGetDto.setImageUrl(auction.getImageUrl());
         auctionGetDto.setSlackUrl(auction.getSlackThreadLink());
         auctionGetDto.setSupplier(auction.getSupplierName());
         auctionGetDto.setWinner(auction.getWinner());
-        auctionGetDto.setPrice(auction.getCurrentBid().toString());
+        auctionGetDto.setPrice(auction.getCurrentBid() == null ? null : auction.getCurrentBid().toString());
         auctionGetDto.setIsFollowed(auction.getFollowers() != null && auction.getFollowers().contains(userEmail));
         auctionGetDto.setIsSupplier(userEmail != null && userEmail.equals(auction.getSupplierEmail()));
         auctionGetDto.setStatus(determineStatus(auction));
