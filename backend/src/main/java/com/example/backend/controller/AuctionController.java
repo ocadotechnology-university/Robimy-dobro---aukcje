92a24b9d3135c1c08dcb51b5755f65f61100d0a4
Bartolomeooo <bartoszkawiak@gmail.com>
2025-04-26 00:18:10 +0200

    Use List instead of ArrayList in method signatures

--- backend/src/main/java/com/example/backend/controller/AuctionController.java
+++ backend/src/main/java/com/example/backend/controller/AuctionController.java
@@ -8,7 +8,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
-import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/auctions")
@@ -30,7 +30,10 @@ public class AuctionController {
     }
 
     @GetMapping
-    public ResponseEntity<?> findAllFilteredAuctions(@RequestParam ArrayList<String> statuses, @RequestParam Boolean myAuctions, @RequestParam Boolean followed, @RequestParam ArrayList<String> dates) throws IOException {
+    public ResponseEntity<?> findAllFilteredAuctions(@RequestParam List<String> statuses,
+                                                     @RequestParam Boolean myAuctions,
+                                                     @RequestParam Boolean followed,
+                                                     @RequestParam List<String> dates) throws IOException {
         try {
             String userEmail = SecurityContextHolder.getContext().getAuthentication().getPrincipal().toString();
             return ResponseEntity.ok(auctionService.getFilteredAuctions(statuses, myAuctions, followed, dates, userEmail));
--- backend/src/main/java/com/example/backend/repository/AuctionRepository.java
+++ backend/src/main/java/com/example/backend/repository/AuctionRepository.java
@@ -1,16 +1,14 @@
 package com.example.backend.repository;
 
 import com.example.backend.model.Auction;
-//import org.springframework.data.repository.NoRepositoryBean;
 import org.springframework.stereotype.Repository;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
 
 @Repository
 public interface AuctionRepository {
     void save(Auction auction) throws IOException;
 
-    List<Auction> findAllByFiltersAndUser(ArrayList<String> statuses, Boolean myAuctions, Boolean followed, ArrayList<String> dates, String userEmail) throws IOException;
+    List<Auction> findAllByFiltersAndUser(List<String> statuses, Boolean myAuctions, Boolean followed, List<String> dates, String userEmail) throws IOException;
 }
