8c049ea64b1d87cf2d8acbc77e3a1ecc9bb3ec44
Bircheses <kbrzoza03@outlook.com>
2025-04-28 01:18:42 +0200

    Add onStart method for fetching cache from google disk

--- backend/src/main/java/com/example/backend/service/ImageCacheService.java
+++ backend/src/main/java/com/example/backend/service/ImageCacheService.java
@@ -1,13 +1,32 @@
 package com.example.backend.service;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.stereotype.Service;
 
+import java.io.IOException;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 @Service
 public class ImageCacheService {
     private final Map<String, byte[]> cache = new ConcurrentHashMap<>();
+    private final GoogleDriveService googleDriveService;
+
+    public ImageCacheService(GoogleDriveService googleDriveService) {
+        this.googleDriveService = googleDriveService;
+    }
+
+    @PostConstruct
+    public void onStart(){
+        try {
+            Map<String, byte[]> images = googleDriveService.downloadAllFiles();
+            for (Map.Entry<String, byte[]> entry : images.entrySet()) {
+                cache.put(entry.getKey(), entry.getValue());
+            }
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
+        }
+    }
 
     public byte[] get(String fileId) {
         return cache.get(fileId);
