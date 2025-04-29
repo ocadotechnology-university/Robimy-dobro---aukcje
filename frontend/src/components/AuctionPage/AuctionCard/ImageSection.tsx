0bdae51a851bbf6fd1622b54cd12d02b1fe86fde
Bircheses <kbrzoza03@outlook.com>
2025-04-29 12:26:46 +0200

    Add blobUrl to ImageSection.tsx for proper visualization

--- frontend/src/components/AuctionPage/AuctionCard/ImageSection.tsx
+++ frontend/src/components/AuctionPage/AuctionCard/ImageSection.tsx
@@ -1,22 +1,29 @@
 import React from "react";
 import {Box, CardMedia, Grid2} from "@mui/material";
 import {ImageWrapperStyle, ImageStyle} from "./AuctionCard.styles";
+import {useGetImages} from "../../../hooks/useGetImages";
 
 type Props = {
     imageUrl: string;
 };
 
-const ImageSection = ({imageUrl}: Props) => (
-    <Grid2 size={{xs: 12, md: 3}} sx={ImageWrapperStyle}>
-        <Box height="100%">
-            <CardMedia
-                component="img"
-                image={`http://localhost:8080/images/${imageUrl}`}
-                alt="Auction item"
-                sx={ImageStyle}
-            />
-        </Box>
-    </Grid2>
-);
+const ImageSection = ({imageUrl}: Props) => {
+    const { data: blobUrl, isLoading } = useGetImages(imageUrl);
+
+    return (
+        <Grid2 size={{xs: 12, md: 3}} sx={ImageWrapperStyle}>
+            <Box height="100%">
+                {!isLoading && blobUrl && (
+                    <CardMedia
+                        component="img"
+                        image={blobUrl}
+                        alt="Auction item"
+                        sx={ImageStyle}
+                    />
+                )}
+            </Box>
+        </Grid2>
+    );
+}
 
 export default ImageSection;
