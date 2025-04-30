33b95153318e97792d16110456673d6757119d91
Bircheses <kbrzoza03@outlook.com>
2025-04-30 18:07:01 +0200

    Add staleTime equal to 5 minutes to useQuery which prevents refetching the image every time

--- frontend/src/hooks/useGetImages.tsx
+++ frontend/src/hooks/useGetImages.tsx
@@ -4,6 +4,7 @@ import { fetchImage } from '../services/fetchImage';
 export const useGetImages = (fileId: string) => {
     return useQuery({
         queryKey: ['image', fileId],
-        queryFn: async () => await fetchImage(fileId)
+        queryFn: async () => await fetchImage(fileId),
+        staleTime: 1000 * 60 * 5
     });
 };
