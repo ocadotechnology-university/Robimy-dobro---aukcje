ba7de5882f1368ed225d2e88af9230aeb150cf2d
Bircheses <kbrzoza03@outlook.com>
2025-05-28 11:12:01 +0200

    Add refresh controller that will try to make accessToken with refreshToken

--- backend/src/main/java/com/example/backend/controller/AuthController.java
+++ backend/src/main/java/com/example/backend/controller/AuthController.java
@@ -1,26 +1,30 @@
 package com.example.backend.controller;
 
 import com.example.backend.constants.CustomException;
-import com.example.backend.security.JwtTokenProvider;
 import com.example.backend.service.GoogleAuthService;
 import com.example.backend.security.Role;
 import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
 
 import static com.example.backend.constants.ErrorMessages.WRONG_EMAIL;
+import static com.example.backend.security.JwtTokenProvider.generateAccessToken;
 
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
+    private static final Logger logger = LoggerFactory.getLogger(AuthController.class);
+
     static final String COMPANY_NAME = "pwr.edu";
 
     @Autowired
@@ -39,7 +43,7 @@ public class AuthController {
             }
 
             Role role = googleAuthService.getUserRoleByEmail(verifiedToken.getEmail());
-            String accessToken = JwtTokenProvider.generateAccessToken(verifiedToken, role);
+            String accessToken = generateAccessToken(verifiedToken, role);
             Map<String, String> tokenResponse = new HashMap<>();
             tokenResponse.put("accessToken", accessToken);
 
@@ -48,6 +52,26 @@ public class AuthController {
         } catch (CustomException e) {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(e.getMessage());
         }
+    }
+
+    @GetMapping("/refresh")
+    public ResponseEntity<?> refresh(@RequestHeader("Authorization") String token, HttpServletRequest request, HttpServletResponse response) {
+        Cookie[] cookies = request.getCookies();
+        for(Cookie cookie : cookies) {
+            if(cookie.getName().equals("refreshToken")) {
+                String refreshToken = cookie.getValue();
+                try{
+                    String accessToken = generateAccessToken(refreshToken);
+                    response.setHeader("Authorization", "Bearer " + accessToken);
+                    return ResponseEntity.ok(accessToken);
+                }catch (Exception e) {
+                    logger.info("Refresh token was expired");
+                    return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(e.getMessage());
+                }
+            }
+        }
+        logger.info("Refresh token not found");
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
     }
 
 }
