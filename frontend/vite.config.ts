d20aabe139c4912a59fe7ae2b15879468052da16
Agata Drapala <agata.drapala@ocado.com>
2025-05-29 20:13:00 +0200

    added linter and workflow

---
+++ frontend/vite.config.ts
@@ -0,0 +1,21 @@
+import { defineConfig } from 'vitest/config'
+import react from '@vitejs/plugin-react-swc'
+import eslint from 'vite-plugin-eslint';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    base: '/',
+    plugins: [react(), eslint()],
+    test: {
+        globals: true,
+        environment: 'jsdom',
+        setupFiles: './src/setupTests.ts',
+        css: true,
+        reporters: ['verbose'],
+        coverage: {
+            reporter: ['text', 'json', 'html'],
+            include: ['src/**/*'],
+            exclude: [],
+        }
+    },
+})
