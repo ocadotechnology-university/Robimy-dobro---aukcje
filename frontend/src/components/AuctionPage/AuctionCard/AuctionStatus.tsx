c32067c1e0ff8c12ae6e34c9459193776c3b750e
Bartolomeooo <bartoszkawiak@gmail.com>
2025-06-03 20:26:14 +0200

    Add animated dots for 'IN_PROGRESS' status

--- frontend/src/components/AuctionPage/AuctionCard/AuctionStatus.tsx
+++ frontend/src/components/AuctionPage/AuctionCard/AuctionStatus.tsx
@@ -1,6 +1,7 @@
 import React from "react";
-import {Box, Typography} from "@mui/material";
-import {getStatusLabel} from "./helpers";
+import { Box, Typography } from "@mui/material";
+import { getStatusLabel } from "./helpers";
+import Dot from "../../common/Dot";
 
 type Props = {
     status: string;
@@ -8,13 +9,27 @@ type Props = {
     winner: string;
 };
 
-const AuctionStatus = ({status, supplier, winner}: Props) => {
+const AuctionStatus = ({ status, supplier, winner }: Props) => {
     const statusLabel = getStatusLabel(status);
+    const isLive = status === "IN_PROGRESS";
+
     return (
         <Box display="flex" flexDirection="column" justifyContent="flex-end" flexGrow={1} paddingBottom={1}>
             <Typography variant="body2">
-                Status licytacji: <b>{statusLabel}</b><br/>
-                Dostawca: <b>{supplier}</b><br/>
+                Status licytacji:{" "}
+                <b style={{ color: isLive ? "green" : "inherit", display: "inline-flex", alignItems: "center" }}>
+                    {statusLabel}
+                    {isLive && (
+                        <Box display="flex" ml={0.2}>
+                            <Dot/>
+                            <Dot/>
+                            <Dot/>
+                        </Box>
+                    )}
+                </b>
+                <br />
+                Dostawca: <b>{supplier}</b>
+                <br />
                 Zwycięzca: <b>{winner}</b>
             </Typography>
         </Box>
---
+++ frontend/src/components/common/Dot.tsx
@@ -0,0 +1,25 @@
+import { styled, keyframes } from '@mui/system';
+
+const fadeDot = keyframes`
+  0% { opacity: 0; }
+  30% { opacity: 1; }
+  60% { opacity: 0; }
+  100% { opacity: 0; }
+`;
+
+const Dot = styled('span')({
+    width: 3,
+    height: 3,
+    margin: '0 1px',
+    borderRadius: '50%',
+    backgroundColor: 'green',
+    opacity: 0,
+    transform: 'translateY(3px)',
+    animation: `${fadeDot} 1.5s infinite`,
+
+    '&:nth-of-type(1)': { animationDelay: '0s' },
+    '&:nth-of-type(2)': { animationDelay: '0.3s' },
+    '&:nth-of-type(3)': { animationDelay: '0.6s' },
+});
+
+export default Dot;
