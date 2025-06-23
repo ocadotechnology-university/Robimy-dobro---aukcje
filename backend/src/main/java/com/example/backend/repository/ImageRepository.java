fa7ecdddfa8d27095b1892c9ea116d8f46f9d7f3
Bircheses <kbrzoza03@outlook.com>
2025-06-23 17:21:25 +0200

    Minor fix

--- backend/src/main/java/com/example/backend/repository/ImageRepository.java
+++ backend/src/main/java/com/example/backend/repository/ImageRepository.java
@@ -1,10 +1,12 @@
 package com.example.backend.repository;
 
 import com.example.backend.model.ImageData;
+import org.springframework.stereotype.Repository;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 
+@Repository
 public interface ImageRepository {
     String save(MultipartFile file) throws IOException;
     ImageData get(String fileid) throws Exception;
